ce0019c4c17d8179e90fb13c16581ee00e8b1317
dudinha818 <rosa.santos18@escola.pr.gov.br>
2025-04-22 11:56:37 -0300

    Create sistema-livraria.sql

---
+++ sistema-livraria.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY,
+    nomeCliente VARCHAR(255) NOT NULL,
+    emailCliente VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE Compras (
+    CompraID INT PRIMARY KEY,
+    ClienteID INT,
+    NomeLivro VARCHAR(255) NOT NULL,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID) ON DELETE CASCADE
+);
