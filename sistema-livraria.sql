5adbde6e5fc902e795dea362b8e5614706909292
dudinha818 <rosa.santos18@escola.pr.gov.br>
2025-04-22 12:01:42 -0300

    Update sistema-livraria.sql

--- sistema-livraria.sql
+++ sistema-livraria.sql
@@ -10,3 +10,21 @@ CREATE TABLE Compras (
     NomeLivro VARCHAR(255) NOT NULL,
     FOREIGN KEY (ClienteID) REFERENCES Clientes(ID) ON DELETE CASCADE
 );
+
+SELECT * FROM Clientes; 
+
+-- Inserindo dados na tabela Clientes
+INSERT INTO Clientes (ID, nomeCliente, emailCliente)
+VALUES 
+    (1, 'Alice Silva', 'alice.silva@email.com'),
+    (2, 'Bruno Costa', 'bruno.costa@email.com'),
+    (3, 'Carla Souza', 'carla.souza@email.com');
+
+-- Inserindo dados na tabela Compras
+INSERT INTO Compras (CompraID, ClienteID, NomeLivro)
+VALUES 
+    (1, 1, 'O Pequeno Príncipe'),
+    (2, 2, 'Dom Casmurro'),
+    (3, 3, '1984'),
+    (4, 1, 'Cem Anos de Solidão');
+
