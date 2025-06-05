74a416563a5e191b8a3ee2182dc271f1c711f7cd
dudinha818 <rosa.santos18@escola.pr.gov.br>
2025-06-05 11:51:37 -0300

    Create intervalo.py

---
+++ intervalo.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que insira um número inteiro
+numero = int(input("Digite um número inteiro: "))
+
+# Gera a sequência de números de 0 até o número fornecido
+sequencia = list(range(numero + 1))
+
+# Exibe a sequência gerada
+print("Sequência gerada:", sequencia)
