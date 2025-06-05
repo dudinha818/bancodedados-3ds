606f1d09c3b7884977491f90cc504b09c0971549
dudinha818 <rosa.santos18@escola.pr.gov.br>
2025-06-05 12:01:11 -0300

    Create contagem.py

---
+++ contagem.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que insira uma frase
+frase = input("Digite uma frase: ")
+
+# Conta o número de caracteres na frase
+quantidade_caracteres = len(frase)
+
+# Exibe o resultado
+print(f"A frase contém {quantidade_caracteres} caracteres.")
